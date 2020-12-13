3c5bdcb6d15c8b72c4a626f5684d732f80cecaa9
AtlanPrime <zilliondollarkuruvi@gmail.com>
2020-12-13 06:38:31 +0000

    Revert "sm6250-common: import doze from sdm660-common"
    This reverts commit de68b0702baa2fbd655b7f57082a2d59579c32b0.

--- common.mk
+++ common.mk
@@ -160,10 +160,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     vendor.qti.hardware.display.allocator-service
 
-# Doze
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
